096af22ce868b3d1a394238d6048e7e2a9d44bdb
satra77 <satrapech@gmail.com>
2026-06-17 20:01:44 +0700

    done

---
+++ sql_project/5_optimal_skills.sql
@@ -0,0 +1,30 @@
+WITH skills_demand AS(
+SELECT
+    skill_id,
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+    job_work_from_home = TRUE
+GROUP BY
+    skill_id
+)
+
+WITH average_salary AS(
+SELECT 
+    skills,
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
+GROUP BY
+    skill_id
+)    
